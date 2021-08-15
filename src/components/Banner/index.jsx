6c14f229ee670f17a97ed4e3378e60c482de0ccc
thanhhoang <maketwithxunu@gmail.com>
2021-08-15 17:09:51 +0700

    hello

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -7,14 +7,7 @@ export const Banner = () => {
         <div className={styles.wrapper}>
             <Container>
                 <div className={`${styles.mainContent} flexSpacebetween`}>
-                    {/* <img src="images/posterImage.png" alt="bannerImage" /> */}
                     <div className={`${styles.bannerContent}`}>
-                        {/* <h3>Movie Poster</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus facilisis ullamcorper egestas diam egestas sed placerat diam commodo. Amet et nisl a nam eget vitae. Integer ac id sed vulputate nunc amet. Nulla tincidunt sit odio amet porttitor. At auctor</p>
-                        <div className={styles.btn}>
-                            <img src="images/circlePlay.png" alt=""/>
-                            <p>Xem phim</p>
-                        </div> */}
                     </div>
                 </div>
             </Container>
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,40 +5,41 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
     return (
         <div className={styles.main}>
+            <div className={styles.ortherPage}>
+                <span>Fullphim.net</span>
+                <span>phimmoi.net</span>
+                <span>banhtv</span>
+                <span>phim bất hủ</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+                <span>Fullphim.net</span>
+            </div>
             <Container className={styles.wrapper}>
-                <div className={styles.footerFlex}>
-                    <div className={styles.flexLeft}>
-                        <h3>VT Movie</h3>
-                        <div className={styles.image}>
-                            <img src="images/footer1.png" alt="" />
-                        </div>
-                        <div className={styles.image}>
-                            <img src="images/footer2.png" alt="" />
-                        </div>
-                    </div>
-                    <div className={styles.flexMid}>
-                        <p>Quy định</p>
-                        <p>Điều khoản sử dụng</p>
-                        <p>Chính sách Và Quy định chung</p>
-                        <p>Chính sách về Quyền riêng tư</p>
-                        <p>Chính  sách về Sở hữu trí tuệ</p>
-                    </div>
-                    <div className={styles.flexRight}>
-                        <p>Trợ Giúp</p>
-                        <p>FAQs</p>
-                        <p>Liên hệ</p>
-                        <p>Góp ý</p>
-                    </div>
-                </div>
-            </Container>
-
-            <div className={styles.border}></div>
-            <Container>
-                <div className={styles.ourCompany}>
-                    <p>Công ty cổ phần phát triển VT Movie</p>
-                    <p>Email: support@vtmovie.vn | Hotline :  012.345.678 ( Miễn phí)</p>
-                    <p>Giấy phép Cung cấp Dịch vụ Phát thanh, Truyền hình trả tiền số 192/GP-BTTTT cấp ngày 02/8/2021.</p>
-                </div>
+                <img src="/images/vtmovie.png" alt="logo" />
+                <p>
+                    Copyright ® 2021 VT Movie ver 1.0 All Rights Reserved.
+                    VT Movie - xem phim hay chất lượng full hd
+                    Liên hệ: Viethoangquoc2312@gmail.com
+                    <br />sitemap | contact
+                </p>
+                
             </Container>
         </div>
     );
